105b5d537fc3a04d1c9676255af0da83b8457b6a
deciduously <ben@deciduously.com>
2019-03-05 08:23:44 -0500

    grid

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Welcome to minesweeper.')
+const buildGrid = (size) => {
+    const ret = []
+    for (let i = 0; i < size; i++) {
+        const row = []
+        for (let j = 0; j < size; j++) {
+            row.push(false)
+        }
+        ret.push(row)
+    }
+    return ret
+}
+
+function Grid(size) {
+    this.grid = buildGrid(size)
+
+    this.toString = () => {
+        let ret = ""
+        for (let row = 0; row < this.grid.length; row++) {
+            for (let col = 0; col < this.grid.length; col++) {
+                let char = this.grid[row][col] ? '*' : '.'
+                ret += char
+            }
+            ret += '\n'
+        }
+        return ret
+    }
+}
+
+const g = new Grid(5)
+
+console.log('Welcome to minesweeper.\nGrid:\n' + g)
